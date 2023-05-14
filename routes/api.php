ffcd2d32b4273fc62bd2c9ccc1bb9f67082fc29e
Carlos Eduardo Dos Santos <eduardo191949@hotmail.com>
2023-05-14 20:28:42 -0300

    remocao  de rota

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::get('/cep/{cep}', [CustomerController::class, 'getCep']);
-
-
-Route::get('/customer/{csv}', [CustomerController::class, 'store'])->name('csv.customer');
